22db6c4b742516bcbd98c158d62f9588d1053232
Goro7777 <gor.mainaddress@gmail.com>
2025-06-08 17:47:01 +0300

    Added validation to sign-up

--- index.js
+++ index.js
@@ -1,6 +1,64 @@
 const path = require("node:path");
 const express = require("express");
 require("dotenv").config();
+const { body, validationResult } = require("express-validator");
+const { error } = require("node:console");
+const { values } = require("lodash");
+
+/**
+ * ---------------------- USERS ----------------------
+ */
+
+const users = [
+    {
+        username: "bobo",
+        firstname: "John",
+        lastname: "Smith",
+        email: "play@mail.ru",
+        password: "123456",
+    },
+];
+
+/**
+ * ---------------------- VALIDATION ----------------------
+ */
+
+const validateUserSignup = [
+    body("username")
+        .trim()
+        .custom((value) => {
+            const usernameFoundInDb = users.find(
+                (user) => user.username === value
+            );
+            if (usernameFoundInDb) {
+                throw new Error("Username already in use");
+            }
+            return true;
+        }),
+    body("email")
+        .trim()
+        .isEmail()
+        .withMessage("Not a valid e-mail address")
+        .custom((value) => {
+            const emailFoundInDb = users.find((user) => user.email === value);
+            if (emailFoundInDb) {
+                throw new Error("E-mail already in use");
+            }
+            return true;
+        }),
+
+    body("password")
+        .trim()
+        .isLength({ min: 6 })
+        .withMessage("Must be at least 6 characters long"),
+    body("confirmPassword")
+        .trim()
+        .custom((value, { req }) => {
+            if (value !== req.body.password)
+                throw new Error('Must match the "Password" field');
+            return true;
+        }),
+];
 
 const app = express();
 
@@ -48,12 +106,30 @@ app.get("/sign-up", (req, res) => {
     res.render("pages/sign-up", {
         title: "Sing-up",
         links: [],
+        errors: {},
+        values: {},
     });
 });
 
-app.post("/sign-up", (req, res) => {
-    console.log("you've signed up");
-    res.redirect("/");
+app.post("/sign-up", validateUserSignup, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        errorValues = Object.fromEntries(
+            errors.errors.map((error) => [error.path, error.msg])
+        );
+        console.log("--------------------");
+        console.log(errorValues);
+        res.render("pages/sign-up", {
+            title: "Sing-up",
+            links: [],
+            values: req.body,
+            errors: errorValues,
+        });
+    } else {
+        console.log("you've signed up");
+        console.log(req.body);
+        res.redirect("/");
+    }
 });
 
 // catch-all middleware for handling errors
