3bd0c71d1fc5e93ac693b5d3b95728c26d1a20a2
Goro7777 <gor.mainaddress@gmail.com>
2025-06-08 14:56:11 +0300

    Added login page

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(
     )
 );
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
     res.render("pages/index", {
         title: "Club House",
         links: [
@@ -24,6 +24,21 @@ app.use("/", (req, res) => {
             { href: "/signup", text: "Sign Up" },
         ],
     });
+});
+
+app.get("/login", (req, res) => {
+    // what if the user is already logged in?
+    console.log("login page");
+    res.render("pages/login", {
+        title: "Login",
+        links: [],
+    });
+});
+
+app.post("/login", (req, res) => {
+    // what if the user is already logged in?
+    console.log("form has been submitted");
+    res.redirect("/");
 });
 
 // catch-all middleware for handling errors
